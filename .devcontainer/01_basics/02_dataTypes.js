25fab8b1610224ff2abbe1fba9f231df4a43f95c
Saurabh Pote <59841363+Saurabhpote@users.noreply.github.com>
2024-03-18 18:08:35 +0000

    add: datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+// premitive 
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique 
+
+
+//object 
+
+
+console.log(typeof"saurabh")
+
+let age = 20
+let isLoggedIn = false
