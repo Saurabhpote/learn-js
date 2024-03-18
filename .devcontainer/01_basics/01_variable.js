7bae797513ace55b4db50ecd9a0a19ff3915b467
Saurabh Pote <59841363+Saurabhpote@users.noreply.github.com>
2024-03-18 17:51:43 +0000

    add: varibales

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const myID = 1234
+let myEmail = "saurabhpote96@gmail.com"
+var myPass = "1234"
+myTown =  "kolhapur"
+
+
+/* Prefer not to use var 
+because of issue in block scope and funcional scope
+*/
+
+console.log(myID,myEmail,myPass,myTown)
